fedfdf01520af68953bb316a013c838542544bb5
Evan Lucas <evanlucas@me.com>
2013-10-22 03:06:48 -0500

    Added minified client side js

---
+++ public/js/min/style.js
@@ -0,0 +1,6 @@
+/**
+ * curapps.com v0.0.4
+ * Copyright 2013 Evan Lucas
+ * Licensed under the MIT license
+ */
+$(document).ready(function(){$(".nav-collapse").on("shown",function(){$(".btn-navbar i").removeClass("icon-chevron-down").addClass("icon-chevron-up")}),$(".nav-collapse").on("hidden",function(){$(".btn-navbar i").removeClass("icon-chevron-up").addClass("icon-chevron-down")}),$("[rel=tooltip]").tooltip()});
