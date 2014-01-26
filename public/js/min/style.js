1201d502ec734257192c367580f100f6eb332c3d
Evan Lucas <evanlucas@me.com>
2014-01-26 12:35:38 -0600

    Updated minified client side js

--- public/js/min/style.js
+++ public/js/min/style.js
@@ -1,6 +1,6 @@
 /**
- * curapps.com v0.1.4
- * Copyright 2013 Evan Lucas
+ * curapps.com v0.1.5
+ * Copyright 2014 Evan Lucas
  * Licensed under the MIT license
  */
 $(document).ready(function(){$(".nav-collapse").on("shown",function(){$(".btn-navbar i").removeClass("icon-chevron-down").addClass("icon-chevron-up")}),$(".nav-collapse").on("hidden",function(){$(".btn-navbar i").removeClass("icon-chevron-up").addClass("icon-chevron-down")}),$("[rel=tooltip]").tooltip()});
--- public/js/min/support.js
+++ public/js/min/support.js
@@ -1,6 +1,6 @@
 /**
- * curapps.com v0.1.4
- * Copyright 2013 Evan Lucas
+ * curapps.com v0.1.5
+ * Copyright 2014 Evan Lucas
  * Licensed under the MIT license
  */
 $(document).ready(function(){var a=io.connect("/");$("#submitBtn").click(function(b){b.preventDefault();var c=$("input[name=name]").val(),d=$("input[name=phone]").val(),e=$("input[name=email]").val(),f=$("textarea#desc").val();return""==c?($("#name-cg").addClass("error"),alertify.error("Name is required"),!1):($("#name-cg").removeClass("error"),""==e?($("#email-cg").addClass("error"),alertify.error("Email is required"),!1):($("#email-cg").removeClass("error"),$(this).button("loading"),a.emit("getSupport",{name:c,phone:d,email:e,desc:f}),void 0))}),a.on("getSupportError",function(a){alertify.error(a),~a.indexOf("Invalid email")&&$("input[name=email]").closest(".control-group").addClass("error"),$("#submitBtn").button("reset")}),a.on("getSupportSuccess",function(a){alertify.success(a),$("input[name=email]").closest(".control-group").removeClass("error"),$("#submitBtn").button("reset")})});
